a4dd3377d472637d3c90035f5a2c3760122eb546
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2021-03-06 23:10:51 +0100

    export DISPLAY var

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ mkdir -p build && cd build
 cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . --config Debug -- -j $(nproc)
+
+export DISPLAY=:0
+
 # Test
 ctest -j $(nproc) --output-on-failure -VV
 
