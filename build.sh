635c6d959bbc2166af058a38d9ea53c369526b88
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2021-03-09 18:19:08 +0100

    changed xvfb-invocation

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . --config Debug -- -j $(nproc)
 
-Xvfb :99 &
-export DISPLAY=:99
-
 # Test
-ctest -j $(nproc) --output-on-failure -VV
+xvfb-run ctest -j $(nproc) --output-on-failure -VV
 
