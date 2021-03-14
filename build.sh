299cf0750b1454a96f5c83d29aaed25f8c380f0d
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2021-03-14 12:14:05 +0100

    conditional build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,17 @@ set -euo pipefail
 
 mkdir -p build && cd build
 
+WORK_BUILD_DIR=.
+
+if [ -z ${TRAVIS_BUILD_DIR+x}]; then
+    WORK_BUILD_DIR=${TRAVIS_BUILD_DIR};
+fi
+
+if [ -z ${GITHUB_WORKSPACE+x}]; then
+   WORK_BUILD_DIR=${GITHUB_WORKSPACE};
+fi
+
+
 # Configure
 cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
@@ -13,7 +24,7 @@ chmod u+x tools/img_compare.py
 chmod u+x tools/img_diff.py
 chmod u+x tools/img_crop.py
 python3 -mvenv venv
-./venv/Scripts/pip install -r requirements.txt
+"${WORK_BUILD_DIR}/venv/Scripts/pip" install -r requirements.txt
 # Test
 xvfb-run ctest -j $(nproc) --output-on-failure -VV
 
